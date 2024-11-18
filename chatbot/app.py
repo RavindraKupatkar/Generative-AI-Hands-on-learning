a21d0dd394b4b5837082e8da8386d7d9e12b1272
Raviii09 <66521883+RavindraKupatkar@users.noreply.github.com>
2024-11-18 17:07:14 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -7,12 +7,12 @@ import os
 from dotenv import load_dotenv
 
 import os
-os.environ["OPENAI_API_KEY"] = "sk-WcNbA8gDXwsE0G10eq_sZX3zJDFq7wJtb8Had0zhJBT3BlbkFJF6-ayLj-4SeMyIroDVLYv4uHEAhvt3j0FFBIG_BkYA"
+os.environ["OPENAI_API_KEY"] = "Your openai API key"
 # os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 ## Langmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 import os
-os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["LANGCHAIN_API_KEY"] = "Your Langchain API key"
 # os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
 ## Prompt Template
